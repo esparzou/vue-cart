05a6d451b57e3f0874fd21665e3f172bca0c3efb
esparzou <esparzou@gmail.com>
2019-07-15 06:35:57 +0200

    Init template vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+import ProductsList from './components/ProductsList.vue'
+import Cart from './components/Cart.vue'
+
+export default {
+
+  components: {
+    ProductsList,
+    Cart
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
